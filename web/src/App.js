ea513d7c7ae54c3c46268624f90f0563ccf536a8
nthoang <thoang3377@gmail.com>
2023-01-15 08:09:48 +0000

    import mui

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './views/Home.js'
 
-const App = () => <div></div>
+const App = () => <div>
+    <Routes>
+        <Route path='/' element = {<Home />}/>
+    </Routes>
+</div>
 
 export default App
