7a788827157ee170e4f170caeffc28f9a4818c77
nthoang <thoang3377@gmail.com>
2023-01-19 14:12:44 +0000

    end of the day

--- web/src/views/Home.js
+++ web/src/views/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Button, TextField, Card, CardMedia, Typography} from '@mui/material'
+import { Box, Button, TextField, Card, Typography} from '@mui/material'
 import products from '../images/products'
 import logo from './logo.png'
 
@@ -16,9 +16,9 @@ const Home = () => {
                 <Box sx={{display: "flex", flexDirection: 'row', width: "50%", justifyContent: "center"}}>
                     <img src={logo} />
                 </Box>
-                <TextField label='Nhập tên thật'  variant='outlined' />
-                <TextField label='Nhập tên FaceBook'  variant='outlined' />
-                <TextField label='Nhập địa chỉ'  variant='outlined' />
+                <TextField label='Nhập tên thật'  variant='outlined' required />
+                <TextField label='Nhập tên FaceBook'  variant='outlined' required/>
+                <TextField label='Nhập địa chỉ'  variant='outlined' required/>
                 <Button variant='contained' size='large' sx={{height: "50px", m: "25px"}}>Đăng kí</Button>
             </Box>
             <Box sx={{display: "flex", flexDirection: "row"}}>
