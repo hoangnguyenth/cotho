aaec73ca0704c525f57893ce8db0e6e0d22e95a2
nthoang <thoang3377@gmail.com>
2023-01-19 11:42:11 +0000

    reset

---
+++ web/src/products.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+    id:0,
+    name:"Broccoli",
+    price: 3.99,
+    img: "https://vegetable-shop.surge.sh/img/broccoli.jpg"
+},
+{
+    id:1,
+    name:"Carrots",
+    price: 3.99,
+    img: "https://vegetable-shop.surge.sh/img/carrots.jpg"
+},
+{
+    id:1,
+    name:"Beetroot",
+    price: 4.99,
+    img: "https://vegetable-shop.surge.sh/img/beetroot.jpg"
+},
+{
+    id:1,
+    name:"Tomato",
+    price: 3.99,
+    img: "https://vegetable-shop.surge.sh/img/tomato.jpg"
+},
+{
+    id:1,
+    name:"Cucumber",
+    price: 6.99,
+    img: "https://vegetable-shop.surge.sh/img/cucumber.jpg"
+},
+{
+    id:1,
+    name:"Brinjal",
+    price: 7.99,
+    img: "https://vegetable-shop.surge.sh/img/brinjal.jpg"
+},
+{
+    id:1,
+    name:"Cauliflower",
+    price: 8.99,
+    img: "https://vegetable-shop.surge.sh/img/cauliflower.jpg"
+},
+]
